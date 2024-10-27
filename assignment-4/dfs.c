7184e71fce4cbe0b64e6058d718c38a18706a530
Sourajit Majumder <sourajitm19@gmail.com>
2024-10-27 10:41:04 +0530

    dfs using stack

--- assignment-4/dfs.c
+++ assignment-4/dfs.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #define n 6
 
-void dfs(int graph[n][n], int curr, int vis[]) {
-    printf("%d ", curr);
-    vis[curr-1] = 1;
+int stack[n];
+int top = -1;
 
-    for (int i=0; i<n; i++){
-        if (graph[curr-1][i]==1 && vis[i]==0) {
-            dfs(graph, i+1, vis);
+void push(int num) {
+    if (top < n-1) {
+        stack[++top] = num;
+    }
+}
+
+int pop() {
+    if (top >= 0) {
+        return stack[top--];
+    }
+    return -1;
+}
+
+void dfs(int graph[n][n], int start) {
+    int vis[n] = {0};
+    push(start);
+
+    printf("Depth First Search: \n");
+    
+    while (top != -1) {
+        int curr = pop();
+
+        if (vis[curr-1] == 0) {
+            printf("%d ", curr);
+            vis[curr-1] = 1;
+        }
+
+        for (int i=n-1; i>=0; i--) {
+            if (graph[curr-1][i]==1 && vis[i]==0) {
+                push(i+1);
+            }
         }
     }
 }
@@ -21,9 +48,7 @@ int main() {
         {0, 0, 1, 1, 0, 1},
         {0, 0, 0, 1, 1, 0}
     };
-    int vis[n] = {0};
-    
-    printf("Depth First Search : \n");
-    dfs(graph, 1, vis);
+
+    dfs(graph, 1);
     return 0;
 }
